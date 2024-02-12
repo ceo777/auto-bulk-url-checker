148bb3da892890549f621512c52fae1dd18dad52
Oleg Dubnov <61102565+ceo777@users.noreply.github.com>
2024-02-12 04:22:32 +0300

    feat(urls): add url model
    Added mongoose schema for Url model.

--- src/urls/urls.module.ts
+++ src/urls/urls.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { Url, UrlSchema } from './schemas/url.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature()],
+  imports: [MongooseModule.forFeature([{ name: Url.name, schema: UrlSchema }])],
 })
 export class UrlsModule {}
